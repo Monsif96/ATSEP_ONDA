0405f4a97f5933349ddf0300956aa71669ad5ece
Monsif96 <monsif.ghazaui.tab@gmail.com>
2021-09-07 13:55:45 +0100

    static pdf Done

---
+++ HTMLS/essay.js
@@ -0,0 +1,6 @@
+var date = new Date();
+    console.log(date.getDay());
+    console.log(date.getHours());
+    if(date.getDay() === 2 && date.getHours() === 1) {
+        console.log("HELLO WORLD!");
+    }
